2a9e0857cb07d01d1f3da5dd24052c0d67c52d83
albertverso <caralbertverso@gmail.com>
2024-11-08 13:37:46 -0300

    fix

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,13 @@ export default function About() {
         <div className="flex flex-col justify-center items-center text-white gap-5 lg:gap-20 font-semibold lg:py-5 w-full px-5 md:px-20 lg:px-32 xl:px-52">
             <div className="border-t w-full">
             </div>   
-            <div className="flex w-3/4 flex-col items-start justify-center text-lg gap-2">
-                <p>Olá! Meu nome é <strong>Carlos Alberto</strong>, sou desenvolvedor fullstack e moro em <strong>Fortaleza-CE, Brasil.</strong></p>
+            <div className="flex w-3/4 flex-col items-start justify-center text-lg gap-2 border rounded-lg lg:p-10 p-5">
+                <h1 className="text-3xl mb-5">Sobre</h1>
+                <p>
+                Meu nome é <strong>Carlos Alberto</strong>, sou desenvolvedor fullstack e moro em Fortaleza, CE, Brasil. Estou cursando Análise e Desenvolvimento de Sistemas e tenho 2 anos de experiência em desenvolvimento. Minha carreira começou como desenvolvedor web front-end em uma fintech, e, no início de 2024, concluí um curso especializado em desenvolvimento fullstack pela <strong> <a href="http://digitalcollege.com.br" target="_blank" rel="noopener noreferrer"></a>x</strong>, ampliando meus conhecimentos nas áreas de front e back-end. Atualmente, trabalho como desenvolvedor de automações no <strong>Grupo Carmais</strong>, onde aplico minhas habilidades em tecnologia para otimizar processos e criar soluções inovadoras.
+                </p>
+            </div>
+            <div className="flex w-3/4 flex-col items-start justify-center text-lg gap-2 border lg:p-10 p-5">
                 <p> Tenho <strong>2 anos de experiência</strong> em desenvolvimento, começando como desenvolvedor web frontend em uma fintech de pagamento de contas. Durante esse período, fui responsável por desenvolver e melhorar o site, dashboard e aplicativo da empresa, utilizando tecnologias como:</p>
                 <ul className="list-disc ml-5 mb-3">
                     <li>React Native</li>
@@ -18,22 +23,18 @@ export default function About() {
                 <p className="">
                     Atualmente, atuo como desenvolvedor de automações de sistemas para o <strong>Grupo Carmais</strong>, uma das maiores concessionárias do Brasil, localizada no Ceará. Utilizamos <strong>Robotic Process Automation (RPA)</strong> com Python para reduzir consideravelmente a necessidade de os trabalhadores executarem tarefas de grande volume baseadas em regras.
                 </p>
-                <p className="">
-                    Em 2024, iniciei um curso de fullstack, onde melhorei minhas habilidades e ampliei meus conhecimentos em novas tecnologias como:
-                </p>
-                <ul className="list-disc ml-5 mb-3">
-                    <li>React.js</li>
-                    <li>Express</li>
-                    <li>Sequelize</li>
-                    <li>Node.js</li>
-                    <li>Jwt</li>
-                </ul>
                 <p>
                     Além disso, tenho conhecimentos também em tecnologias como:
                 </p>
                 <ul className="list-disc ml-5 mb-3">
                     <li>Django</li>
                     <li>Flask</li>
+                   
+                    <li>React.js</li>
+                    <li>Express</li>
+                    <li>Sequelize</li>
+                    <li>Node.js</li>
+                    <li>Jwt</li>
                     <li>Cloudinary</li>
                     <li>Google AI Generative Language</li>
                     <li>Google Visiont</li>
