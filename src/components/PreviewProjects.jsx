1f1996843fbf8e2cc6a9439a8cb7a674b186ac86
Carlos Alberto <caralbertverso@gmail.com>
2024-11-22 16:15:34 -0300

    project in 98%

--- src/components/PreviewProjects.jsx
+++ src/components/PreviewProjects.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay, Navigation, Pagination, FreeMode, Thumbs } from 'swiper/modules';
+import { Autoplay, Navigation, Thumbs } from 'swiper/modules';
 import IconsTec from "./IconsTec";
-import { BsArrowUpRightSquare, BsArrowUpRightSquareFill, BsGithub } from 'react-icons/bs';
+import { BsArrowUpRightSquare, BsGithub } from 'react-icons/bs';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -17,11 +17,11 @@ export default function PreviewProjects({title,images, description, icons, repo,
     const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
     return (
-        <div className="flex flex-col justify-center items-start overflow-hidden text-white font-semibold w-full text-lg gap-2 border rounded-lg lg:p-10 p-5">
+        <div className="flex flex-col justify-center items-start overflow-hidden text-white font-semibold w-full text-lg border rounded-lg lg:p-10 p-5">
             <h1 className="text-3xl mb-5">{title}</h1>
             <Swiper
                 style={{
-                    '--swiper-navigation-color': '#0ff3',
+                    '--swiper-navigation-color': 'rgba(255, 255, 255, 0.2)',
                     '--swiper-pagination-color': '#fff',
                 }}
                 loop={true}
@@ -33,22 +33,23 @@ export default function PreviewProjects({title,images, description, icons, repo,
                     disableOnInteraction: false,
                 }}
                 modules={[Navigation, Thumbs, Autoplay]}
-                className="mySwiper2"
+                className="mySwiper2 cursor-grab"
             >
                 {images.map((image, index) => (
                     <SwiperSlide key={index}>
                         <img src={image} />
                     </SwiperSlide>
+                    
                 ))}
             </Swiper>
             <Swiper
                 onSwiper={setThumbsSwiper}
                 loop={true}
-                spaceBetween={10}
-                slidesPerView={10}
+                spaceBetween={5}
+                slidesPerView={images.length}
                 watchSlidesProgress={true}
                 modules={[ Thumbs]}
-                className="mySwiper"
+                className="mySwiper cursor-pointer lg:mt-2"
             >
                 {images.map((image, index) => (
                     <SwiperSlide key={index}>
@@ -56,20 +57,26 @@ export default function PreviewProjects({title,images, description, icons, repo,
                     </SwiperSlide>
                 ))}
             </Swiper>
-            <div className='flex'>
+            <div className='flex mt-2'>
                 <p>
                     Descrição: {description}
                 </p>
             </div>
-            <div className="w-full flex flex-col lg:flex-row justify-center items-center lg:items-start gap-4 lg:gap-20">
+            <div className="w-full flex flex-col lg:flex-row justify-center mt-2 items-center lg:items-start gap-4 lg:gap-20">
                 <div className='flex flex-col items-center justify-start gap-6'>
                     <p>Links:</p>
                     <div className='flex flex-row gap-4'>
                         <a href={site} className="group transform hover:scale-110 transition-transform duration-300 flex flex-col gap-2 items-center justify-center" target="_blank" rel="noopener noreferrer">
                             <BsArrowUpRightSquare className="text-white text-4xl"/>
+                            <div className="absolute -bottom-10 left-1/2 transform -translate-x-1/2 p-2 rounded-xl bg-white bg-opacity-60 opacity-0 group-hover:opacity-100 hidden lg:flex items-center justify-center transition-opacity duration-300">
+                                <span className="text-black text-sm uppercase">SITE</span>
+                            </div>
                         </a>
                         <a href={repo} className="group transform hover:scale-110 transition-transform duration-300 flex flex-col gap-2 items-center justify-center" target="_blank" rel="noopener noreferrer">
                             <BsGithub className="text-white text-4xl"/>
+                            <div className="absolute -bottom-10 left-1/2 transform -translate-x-1/2 p-2 rounded-xl bg-white bg-opacity-60 opacity-0 group-hover:opacity-100 hidden lg:flex items-center justify-center transition-opacity duration-300">
+                                <span className="text-black text-sm uppercase">REPOSITORIO</span>
+                            </div>
                         </a>
                     </div>
                 </div>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,29 +4,61 @@ import js from "../assets/iconsPB/javascript.png";
 import python from "../assets/iconsPB/python.png";
 import react from "../assets/iconsPB/react.png";
 import tailwind from "../assets/iconsPB/tailwind.png";
-import typescript from "../assets/iconsPB/typescript.png";
-import kotlin from "../assets/iconsPB/kotlin.png";
 import css from "../assets/iconsPB/css.png";
 import bootstrap from "../assets/iconsPB/bootstrap.png";
 import html from "../assets/iconsPB/HTML.png";
-import selenium from "../assets/iconsPB/selenium.png";
+import t_home_menu from "../assets/prints/todolist/home_menu.png";
+import t_home_page from "../assets/prints/todolist/home.png";
+import t_login_page from "../assets/prints/todolist/login.png";
+import t_task_page from "../assets/prints/todolist/tarefas.png";
+import t_profile_page from "../assets/prints/todolist/perfil.png";
+import t_loading_page from "../assets/prints/todolist/loading.png";
+import d_home_page from "../assets/prints/dripstore/home.png";
+import d_home_page2 from "../assets/prints/dripstore/home_2.png";
+import d_login_page from "../assets/prints/dripstore/login.png";
+import d_products_page from "../assets/prints/dripstore/produtos.png";
+import e_added_product from "../assets/prints/ecommerce/adicionar.png";
+import e_home_page from "../assets/prints/ecommerce/home.png";
+import e_login_page from "../assets/prints/ecommerce/login.png";
+import e_home_menu from "../assets/prints/ecommerce/home_menu.png";
+import e_edit_product from "../assets/prints/ecommerce/editar.png";
+import ia_home_page from "../assets/prints/ia_gemini/home.png";
+import ia_login_page from "../assets/prints/ia_gemini/login.png";
+import ia_register_page from "../assets/prints/ia_gemini/cadastro.png";
+import ia_seach_page from "../assets/prints/ia_gemini/busca.png";
 
 export default function Projects() {
     return (
         <div className="flex relative flex-col justify-center items-center text-white pb-10 w-full px-5 md:px-20 lg:px-32 xl:px-52 gap-10">
             <PreviewProjects 
-                title={"Projeto 1"}
-                images={["https://swiperjs.com/demos/images/nature-1.jpg", "https://swiperjs.com/demos/images/nature-2.jpg", "https://swiperjs.com/demos/images/nature-3.jpg"]}
-                description={ "Teste"} 
-                icons={[django, js, python]}
+                title={"To-do List"}
+                images={[t_login_page, t_home_page, t_home_menu, t_task_page, t_profile_page, t_loading_page]}
+                description={ "Adicionar Descrição"} 
+                icons={[react, js, tailwind, html, css, bootstrap]}
                 repo={"https://github.com/albertverso"}
                 site={"https://github.com/albertverso"}    
             />
             <PreviewProjects 
-                title={"Projeto 2"}
-                images={["https://swiperjs.com/demos/images/nature-1.jpg", "https://swiperjs.com/demos/images/nature-2.jpg", "https://swiperjs.com/demos/images/nature-3.jpg"]} 
-                description={ "Teste"} 
-                icons={[django, js, python]}
+                title={"DripStore"}
+                images={[d_home_page, d_home_page2, d_login_page, d_products_page]} 
+                description={ "Adicionar Descrição"} 
+                icons={[react, js, tailwind, html, css, bootstrap]}
+                repo={"https://github.com/albertverso"}
+                site={"https://github.com/albertverso"}  
+            />
+            <PreviewProjects 
+                title={"Ecommerce"}
+                images={[ e_login_page, e_home_page, e_home_menu, e_added_product, e_edit_product]} 
+                description={ "Adicionar Descrição"} 
+                icons={[python, django, js, tailwind, html, css]}
+                repo={"https://github.com/albertverso"}
+                site={"https://github.com/albertverso"}  
+            />
+            <PreviewProjects 
+                title={"IA Gemini"}
+                images={[ia_home_page, ia_login_page, ia_register_page, ia_seach_page]} 
+                description={ "Adicionar Descrição"} 
+                icons={[python, js, react, html, tailwind, css]}
                 repo={"https://github.com/albertverso"}
                 site={"https://github.com/albertverso"}  
             />
